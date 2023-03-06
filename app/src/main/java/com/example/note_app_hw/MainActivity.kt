85aebdb939768ce2fe2709d198405a18d6c74fe9
killvic <killvic2000@gmail.com>
2023-03-06 16:49:02 +0200

    Added design for main screen and recycler_view_row.xml

--- app/src/main/java/com/example/note_app_hw/MainActivity.kt
+++ app/src/main/java/com/example/note_app_hw/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.adapter = adapter
         adapter.onClick = {
